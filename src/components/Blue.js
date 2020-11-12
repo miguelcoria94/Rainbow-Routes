ed9222b380e50efc85ac977d66775ea210ef1027
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-12 15:16:51 -0600

    set up red and blue

---
+++ src/components/Blue.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Link, NavLink } from "react-router-dom";
+import Indigo from "./Indigo";
+
+const Color = () => (
+  <div>
+    <h2 className="blue">blue</h2>
+    {/* Links here */}
+
+    {/* Routes here */}
+  </div>
+);
+
+export default Color;
---
+++ src/components/Red.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Link, NavLink } from "react-router-dom";
+import Orange from './Orange';
+import Yellow from './Yellow';
+
+const Color = () => (
+  <div>
+    <h2 className="red">red</h2>
+    {/* Links here */}
+
+    {/* Routes here */}
+  </div>
+);
+
+export default Color;
