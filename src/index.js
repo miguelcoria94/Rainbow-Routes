39d7ed007e62b0b82739910c530a6d7821677147
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-12 15:02:48 -0600

    set up DOM

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    ReactDOM.render(
+        <Root />,
+        document.getElementById("root")
+    )
+})
