0efc52f3c4108d18703141eaccdbe83a3c310c5e
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-12 15:37:50 -0600

    add red routes

--- src/components/Rainbow.js
+++ src/components/Rainbow.js
@@ -8,7 +8,21 @@ import Violet from './Violet';
 const Rainbow = () => (
   <div>
     <h1>Rainbow Router!</h1>
-    {/* Your links should go here */}
+    <NavLink to="/red" activeClassName="active">
+      Red Only
+    </NavLink>
+    <NavLink to="/red/orange" activeClassName="active">
+      Add Orange
+    </NavLink>
+    <NavLink to="/red/yellow" activeClassName="active">
+      Add Yellow
+    </NavLink>
+    <NavLink to="/blue" activeClassName="active">
+      Blue Only
+    </NavLink>
+    <NavLink to="/red/yellow" activeClassName="active">
+      Add Yellow
+    </NavLink>
 
     <div id="rainbow">
       <Route path="/red" component={Red} />
