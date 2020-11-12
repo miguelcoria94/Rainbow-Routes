19ee4864ccdc58f205d672e3a8612bfa9fbc6f0e
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-12 15:13:38 -0600

    import colors

--- src/components/Rainbow.js
+++ src/components/Rainbow.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { Route, Link, NavLink } from "react-router-dom";
+import Red from './Red';
+import Green from './Green';
+import Blue from './Blue';
+import Violet from './Violet';
 
 const Rainbow = () => (
   <div>
