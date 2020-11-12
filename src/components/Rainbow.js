c392c688cf583e1a2cf173585c1febc1e25ab2d1
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-12 15:06:48 -0600

    create a components folder

---
+++ src/components/Rainbow.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Link, NavLink } from "react-router-dom";
+
+const Rainbow = () => (
+  <div>
+    <h1>Rainbow Router!</h1>
+    {/* Your links should go here */}
+
+    <div id="rainbow">{/* Your routes should go here */}</div>
+  </div>
+);
+
+export default Rainbow;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
+const Root = () => (
+    <BrowserRouter>
+        
+    </BrowserRouter>
+)
 
 document.addEventListener("DOMContentLoaded", () => {
     ReactDOM.render(
